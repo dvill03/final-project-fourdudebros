01b63df7409e652d83de795426252aca835a45e4
altvictor <vh1601@gmail.com>
2019-11-05 21:08:36 -0800

    Revert "import json data example"
    This reverts commit 58d1cb828f72830046b7530014ce8cf34b0cd930.

--- test_heatmap/test_heatmap.js
+++ test_heatmap/test_heatmap.js
@@ -3,6 +3,7 @@ var margin = {top: 30, right: 30, bottom: 30, left: 30},
   width = 450 - margin.left - margin.right,
   height = 450 - margin.top - margin.bottom;
 
+console.log("hello world");
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
 .append("svg")
@@ -39,7 +40,7 @@ var myColor = d3.scaleLinear()
   .domain([1,100])
 
 //Read the data
-d3.csv("./test_data.csv", function(data) {
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function(data) {
 
   svg.selectAll()
       .data(data, function(d) {return d.group+':'+d.variable;})
