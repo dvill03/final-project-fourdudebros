498e8a2987c98a522742101ae840f18e49de9e5a
altvictor <vh1601@gmail.com>
2019-11-06 18:08:20 -0800

    Revert "Revert "Merge branch 'master' into heatmap-testing""
    This reverts commit cdffb98db959bd19b4101be2a0eedfbfc2d323da.

---
+++ frontend/Sarcix/App/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('run', views.run, name='run'),
+    path('reports', views.reports, name='reports'),
+    path('analysis', views.analysis, name='analysis'),
+    path('compare', views.compare, name='compare'),
+    path('prepare', views.prepare, name='prepare'),
+    path('setup', views.setup, name='setup'),
+    path('help', views.help, name='help'),
+]
---
+++ frontend/Sarcix/App/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from .models import Run
+
+# Create your views here.
+
+def home(request):
+    runs = Run.objects.all()
+    return render(request, 'home.html', {'runs': runs})
+
+def run(request):
+    return render(request, 'run.html')
+
+def reports(request):
+    return render(request, 'reports.html')
+
+def analysis(request):
+    return render(request, 'analysis.html')
+
+def compare(request):
+    return render(request, 'compare.html')
+
+def prepare(request):
+    return render(request, 'prepare.html')
+
+def setup(request):
+    return render(request, 'setup.html')
+
+def help(request):
+    return render(request, 'help.html')
