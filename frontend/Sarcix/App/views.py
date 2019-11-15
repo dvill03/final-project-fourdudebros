4d1823ea0ea0fbcb205a76bf6f510c94e0fb1fc6
altvictor <vh1601@gmail.com>
2019-11-15 01:58:32 -0800

    in-progress with real data
    issues with sizing

--- frontend/Sarcix/App/views.py
+++ frontend/Sarcix/App/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Run
 import psycopg2
+from psycopg2.extras import RealDictCursor
+import json
 
 # Create your views here.
 
@@ -32,11 +34,13 @@ def help(request):
 
 def get_runs(request):
     con = psycopg2.connect(database='sarcix_test_db', user='postgres', password='1601324', host='localhost', port='5432')
-    cur = con.cursor()
-    query_sql = "SELECT row_to_json(row) FROM (SELECT event_name, coverage_name, score FROM run1) row LIMIT 2;" 
+    cur = con.cursor(cursor_factory=RealDictCursor)
+    #query_sql = "SELECT row_to_json(row) FROM (SELECT event_name, coverage_name, score FROM run1) as row LIMIT 5;" 
+    query_sql = "SELECT event_name, coverage_name, score FROM run1 LIMIT 10"
     cur.execute(query_sql)
     results = cur.fetchall()
-    print(results)
+    
+    #return json.dumps(cur.fetchall(), indent=2)
     return JsonResponse(results, safe=False)
 
 def heatmap(request):
